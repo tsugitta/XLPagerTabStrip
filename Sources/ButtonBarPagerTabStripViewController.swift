8c17f7c527e1360b9ebde0788fd9ef9def5a3191
Toshinori Tsugita <tsugitta@gmail.com>
2016-05-20 06:42:57 +0900

    Do not scroll menubar to show selected button in center when the actual button doesn't change. When index shift occurs, the index shifts and currentIndex shifts same amount, therefore the actual button doesn't change. But buttonBarView#moveFromIndex regards it that the index has changed and should make the button on center. This always occurs when scrolling buttons bar, so make it natural by this commit.

--- Sources/ButtonBarPagerTabStripViewController.swift
+++ Sources/ButtonBarPagerTabStripViewController.swift
@@ -219,7 +219,7 @@ public class ButtonBarPagerTabStripViewController: PagerTabStripViewController, 
     
     public func pagerTabStripViewController(pagerTabStripViewController: PagerTabStripViewController, updateIndicatorFromIndex fromIndex: Int, toIndex: Int, withProgressPercentage progressPercentage: CGFloat, indexWasChanged: Bool) {
         guard shouldUpdateButtonBarView else { return }
-        buttonBarView.moveFromIndex(fromIndex, toIndex: toIndex, progressPercentage: progressPercentage, pagerScroll: .Yes)
+        buttonBarView.moveFromIndex(fromIndex, toIndex: toIndex, progressPercentage: progressPercentage, pagerScroll: .Yes, scrollToSelectedButton: true)
         if let changeCurrentIndexProgressive = changeCurrentIndexProgressive {
             let oldCell = buttonBarView.cellForItemAtIndexPath(NSIndexPath(forItem: currentIndex != fromIndex ? fromIndex : toIndex, inSection: 0)) as? ButtonBarViewCell
             let newCell = buttonBarView.cellForItemAtIndexPath(NSIndexPath(forItem: currentIndex, inSection: 0)) as? ButtonBarViewCell
@@ -353,6 +353,6 @@ public class ButtonBarPagerTabStripViewController: PagerTabStripViewController, 
         }
     }
     
-    private var shouldUpdateButtonBarView = true
+    var shouldUpdateButtonBarView = true
     
 }
